443b6ebbf0ad59c04e3689a007596959d7a2a2d2
Prabhat Rana <153556000+prabhatrana666@users.noreply.github.com>
2026-07-17 21:37:40 +0530

    razorpay added wip

--- src/components/paynow/PayNow.jsx
+++ src/components/paynow/PayNow.jsx
@@ -349,7 +349,7 @@ function PayNow() {
                                     )
                                 }
                             >
-                                Pay with 
+                                Pay with Razorpay
                             </button>
 
                         </div>
